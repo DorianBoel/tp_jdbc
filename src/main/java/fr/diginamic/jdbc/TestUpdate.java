22ed94676be5e2dd60647822fcd8bf311a473576
DorianBoel <dorian.boel@free.fr>
2022-07-10 12:17:12 +0200

    add tp3 exo2

--- src/main/java/fr/diginamic/jdbc/TestUpdate.java
+++ src/main/java/fr/diginamic/jdbc/TestUpdate.java
@@ -4,16 +4,16 @@ import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-public class TestInsert {
+public class TestUpdate {
 
 	public static void main(String[] args) {
 		
 		try(Connection connection = DbConnect.connect()) {
 			
 			Statement statement = connection.createStatement();
-			String insert = "INSERT INTO fournisseur (id, nom) VALUES (4, 'La Maison de la Peinture')";
+			String update = "UPDATE fournisseur SET nom = 'La Maison des Peintures' WHERE id = 4";
 			
-			int res = statement.executeUpdate(insert);
+			int res = statement.executeUpdate(update);
 			System.out.println(res > 0);
 			
 		} catch(SQLException e) {
